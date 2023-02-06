1ed17519061d8072ac471cc950cb7d1632e71e9f
nayoseb <nayoseb1018@gmail.com>
2023-02-06 16:06:19 +0900

    .

---
+++ src/main/java/com/codestates/practicegithubActiondelpoy/PracticeGithubActionDelpoyApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondelpoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionDelpoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionDelpoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithubActiondelpoy/PracticeGithubActionDelpoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondelpoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDelpoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
